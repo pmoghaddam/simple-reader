3978c141bf5fd0beaf149e5fb983a50fc8a66277
Payam Moghaddam <payam.moghaddam@accenture.com>
2013-03-13 19:27:02 -0700

    Base Rails 3.x project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleReader::Application.config.secret_token = 'd1c767bc03b36c58f22ea31c909fa2d01c69185a7612676964fd275f051768149a6cbf5fcac07a9d2a3fbb8cf7d00e085946fbe282af30cdcc610840fed9039e'
